f79152d7f3de58af0f60132255bd5b5834f846d9
Sam Fober <36276130+SamFober@users.noreply.github.com>
2025-03-21 20:00:53 +0100

    [FD4-17-BE]: feat: add mail service interface

---
+++ WebApi/Interfaces/Services/IMailService.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApi.Interfaces.Services
+{
+    public interface IMailService
+    {
+        Task<bool> SendEmail(string recipient, string subject, string body, List<object> attachments);
+    }
+}
