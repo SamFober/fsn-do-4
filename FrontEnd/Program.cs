8ddbeb3c6e8c4915b34228e7bd73ce0c0196dfc9
Thomas Rommelse <thomasrommelse1@gmail.com>
2025-03-24 16:41:15 +0100

    Created MobileMenuService

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,4 +1,5 @@
 using FrontEnd;
+using FrontEnd.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<IMobileMenuService, MobileMenuService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5213/") });
 
 await builder.Build().RunAsync();
---
+++ FrontEnd/Services/MobileMenuService.cs
@@ -0,0 +1,24 @@
+﻿namespace FrontEnd.Services
+{
+
+    public interface IMobileMenuService
+    {
+        Task CloseMobileMenu();
+        void RegisterMenuCloseCallback(Action callback);
+    }
+    public class MobileMenuService : IMobileMenuService
+    {
+        private Action? OnMenuClosed;
+
+        public void RegisterMenuCloseCallback(Action callback)
+        {
+            OnMenuClosed = callback;
+        }
+
+        public Task CloseMobileMenu()
+        {
+            OnMenuClosed?.Invoke();
+            return Task.CompletedTask;
+        }
+    }
+}
