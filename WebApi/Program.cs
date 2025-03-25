c2210e4b179526f3c2ca0492e8208665539d61be
Carlos Miguel <carlos.miguel@live.nl>
2025-03-25 21:03:28 +0100

    [FD4-docker] fix: swagger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -86,11 +86,9 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Always enable Swagger for easier API testing
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
